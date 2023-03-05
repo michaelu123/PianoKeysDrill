bfcdb42db37f8d24f790e6e45577914f041aa06e
Michael Uhlenberg <michael.uhlenberg@t-online.de>
2023-03-05 12:49:02 +0100

    trying to obtain service worker

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 const config = {
-	plugins: [sveltekit(), SvelteKitPWA()],
+	plugins: [sveltekit(), SvelteKitPWA({ registerType: 'autoUpdate', strategies: 'generateSW' })],
 };
 
 export default config;
